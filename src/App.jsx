421d8c725c40e5648499769c86701376185bdfc7
JoiZs <pyaesoneaung1126@hotmail.com>
2025-04-26 22:29:38 -0400

    feat(composition_patterns): children as props

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Card } from "./components";
+
+function App() {
+  return (
+    <>
+      {/* Children as Props */}
+      <Card>
+        <h2>Hello World</h2>
+        <p>This is inside a card.</p>
+      </Card>
+    </>
+  );
+}
+
+export default App;
