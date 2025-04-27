8450c6171d8a71cfbea0d8fa61153abe5b72ee27
JoiZs <pyaesoneaung1126@hotmail.com>
2025-04-26 22:58:41 -0400

    feat(styling_in_react): 3 types of styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import SimpleForm from "./components/simpleform";
+import "./App.css";
+import styles from "./App.module.css";
+import styled from "styled-components";
+
+// start here
+const Title = styled.h1`
+  color: tomato;
+  font-size: 2rem;
+  text-align: center;
+`; // end here
 
 function App() {
   return (
     <>
-      <SimpleForm />
+      {/* 1. Regular CSS */}
+      <span>1. Regular CSS</span>
+      <h1 className="title">Hello, world!</h1>
+      <br />
+      <hr />
+      {/* 2. CSS Modules */}
+      <span>2. CSS Modules</span>
+      <h1 className={styles.title}>Hello, CSS Modules!</h1>
+      <br />
+      <hr />
+      {/* 3. Styled-Components */}
+      <span>3. Styled-Components</span>
+      <Title>Hello, Styled-Components!</Title>
     </>
   );
 }
